c92555845ea5c49c1175a202530626fd7adb8eec
Slav <slav@kurilyak.com>
2014-12-06 17:49:38 -0800

    Add better error logs

---
+++ config/initializers/errors.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+
+# Example:
+#   begin
+#     some http call
+#   rescue *HTTP_ERRORS => error
+#     notify_hoptoad error
+#   end
+
+HTTP_ERRORS = [Timeout::Error,
+               Errno::EINVAL,
+               Errno::ECONNRESET,
+               EOFError,
+               Net::HTTPBadResponse,
+               Net::HTTPHeaderSyntaxError,
+               Net::ProtocolError]
