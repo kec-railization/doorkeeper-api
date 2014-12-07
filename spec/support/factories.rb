9bb188177b57923ce194a81791f8ffeeaae2cdee
Slav <slav@kurilyak.com>
2014-12-06 16:23:31 -0800

    Add Users model and integrate factories

---
+++ spec/support/factories.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  sequence :token do
+    SecureRandom.hex(3)
+  end
+
+  sequence(:first_name)     { |n| "first_name #{n}" }
+  sequence(:last_name)      { |n| "last_name #{n}" }
+  sequence(:email)          { |n| "test#{n}@example.com" }
+
+  factory :user do
+    # device_token { generate(:token) }
+    # first_name
+    # last_name
+    # email
+    # password              "foobar123"
+    # password_confirmation "foobar123"
+  end
+end
