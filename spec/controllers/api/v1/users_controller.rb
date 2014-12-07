b353f507a9c9040de7ce52b29917704d643b3369
Slav <slav@kurilyak.com>
2014-12-06 16:13:45 -0800

    Integrate rspec

---
+++ spec/controllers/api/v1/users_controller.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+# describe Api::V1::UsersController do
+#   it 'describe test' do
+#     add test here
+#   end
+# end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+# describe User, 'Validations' do
+#   add test here
+# end
+
+# describe User, 'Associations' do
+#   add test here
+# end
