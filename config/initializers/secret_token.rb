cb5b21eb854be63b858200cc8bb4d0b4ed7a7985
Slav <slav@kurilyak.com>
2014-12-06 16:09:11 -0800

    Add routes for /v1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_token to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_token to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-MygreenspaceApi::Application.config.secret_token = '4bf2cd1849e1dff3be5ae7a983d73a8ecdb8b565087cfe6aab3b19c15ab038e1e77dbc9e0f070a28ae18a82331a9f292733d397f68e8fc4cfb2409391adfd87e'
+MyGreenSpaceAPI::Application.config.secret_token = '4bf2cd1849e1dff3be5ae7a983d73a8ecdb8b565087cfe6aab3b19c15ab038e1e77dbc9e0f070a28ae18a82331a9f292733d397f68e8fc4cfb2409391adfd87e'
